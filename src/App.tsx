5ba54d733571f3d4fac0d67f144d73b882850b92
Shane Porter <shane@freshclickmedia.com>
2019-06-30 10:50:20 +0100

    - created Harrison main component, to be used by App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-import { LocationService } from './services/LocationService';
-import { SunService } from './services/SunService';
-import { HarrisonService } from  './services/HarrisonService';
+import Harrison from './components/Harrison';
 
 const App: React.FC = () => {
-
-  const [sunTimes, setSunTimes] = useState({});
-
-  const fetch = async () => {
-
-    try {
-
-      // @TODO: DI here for composition root:
-      let harrisonService:HarrisonService = new HarrisonService(
-        new LocationService(), new SunService()
-      );
-
-      const res = await harrisonService.getSunTimes();
-
-      setSunTimes(res);
-
-    } catch(e) {
-      console.error('TODO: handle error');
-    }
-  }
-
-  fetch();
-
   return (
-    <div>{JSON.stringify(sunTimes, null, 2)}</div>
+    <div><Harrison />></div>
   );
 }
 
---
+++ src/components/Harrison.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import { LocationService } from '../services/LocationService';
+import { SunService } from '../services/SunService';
+import { HarrisonService } from  '../services/HarrisonService';
+
+const Harrison: React.FC = () => {
+
+  const [sunTimes, setSunTimes] = useState({});
+
+  const fetch = async () => {
+
+    try {
+
+      // @TODO: DI here for composition root:
+      let harrisonService:HarrisonService = new HarrisonService(
+        new LocationService(), new SunService()
+      );
+
+      const res = await harrisonService.getSunTimes();
+
+      setSunTimes(res);
+
+    } catch(e) {
+      console.error('TODO: handle error');
+    }
+  }
+
+  fetch();
+
+  return (
+    <div>{JSON.stringify(sunTimes, null, 2)}</div>
+  );
+}
+
+export default Harrison;
